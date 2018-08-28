02b06c688c84af13b0244211b8d150cced1eee3f
Joshua Patrick <jcpatrick@verisk.com>
2018-08-28 15:05:43 -0600

    Can now switch between calculator screens

--- FIREcalc/CalculatorTypeTableViewController.swift
+++ FIREcalc/CalculatorTypeTableViewController.swift
@@ -12,9 +12,16 @@ import UIKit
 class CalculatorTypeTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var TableView: UITableView!
+    @IBOutlet var BackGroundView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        TableView.delegate = self
+        TableView.dataSource = self
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleBackgroundTapped(_:)))
+        BackGroundView.addGestureRecognizer(tapGesture)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -22,7 +29,20 @@ class CalculatorTypeTableViewController: UIViewController, UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        if(indexPath.row == 0)
+        {
+            cell.textLabel?.text = "When can I retire?"
+        }
+        else if(indexPath.row == 1)
+        {
+            cell.textLabel?.text = "How much do I need to save?"
+        }
+        else if(indexPath.row == 2)
+        {
+            cell.textLabel?.text = "Calculate 20 years"
+        }
+        return cell
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -31,5 +51,27 @@ class CalculatorTypeTableViewController: UIViewController, UITableViewDataSource
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 25
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if(indexPath.row == 0)
+        {
+            let controller = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "WhenToRetireNavController")
+            present(controller, animated: false, completion: nil)
+        }
+        else if(indexPath.row == 1)
+        {
+            let controller = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "HowMuchToRetireNavController")
+            present(controller, animated: false, completion: nil)
+        }
+        else if(indexPath.row == 2)
+        {
+            let controller = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "20YearsNavController")
+            present(controller, animated: false, completion: nil)
+        }
+    }
+    
+    @objc func handleBackgroundTapped(_ sender: UITapGestureRecognizer) {
+        dismiss(animated: false, completion: nil)
     }
 }
